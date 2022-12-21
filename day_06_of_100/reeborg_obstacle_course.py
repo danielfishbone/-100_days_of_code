b72b66e477e381537af6b29ba3973fb58b91ab45
danielfishbone <fishbonedaniel@gmail.com>
2022-12-21 05:17:25 -0800

    day 06 Reeborg Hurdle 3 course

---
+++ day_06_of_100/reeborg_obstacle_course.py
@@ -0,0 +1,25 @@
+"""day 6 of 100 days of code 
+this is the reeborg course
+goto https://https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+to use the reeborg world """
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+    if wall_in_front():
+        jump()
