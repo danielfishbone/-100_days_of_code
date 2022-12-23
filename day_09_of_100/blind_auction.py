b36eb4170dfc63b3d97df0a83f5894cae9ef6092
DanielSamuels <fishbonedaniel@gmail.com>
2022-12-23 07:44:14 -0800

    Create blind_auction.py
    a program that demonstrates an auction

---
+++ day_09_of_100/blind_auction.py
@@ -0,0 +1,22 @@
+import os
+#HINT: You can call clear() to clear the output in the console.
+
+is_bidding =True
+bidders = {}
+highest_bid = 0
+highest_bidder = ""
+while is_bidding:
+  name = input("What is your name?\n").capitalize()
+  amount = int(input("How much are you bidding?\n"))
+  bidders[name] = amount
+
+  if input("Are there any more bidders?, yes or no\n").lower() == "no":
+    is_bidding = False
+  os.system('cls' if os.name == 'nt' else 'clear')    
+for bidder in bidders:
+  if bidders[bidder] > highest_bid:
+     highest_bid = bidders[bidder]
+     highest_bidder = bidder
+
+print(f"{highest_bidder} Won with a ${highest_bid} bid")
+    
