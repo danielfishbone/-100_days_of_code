dd32f6f6e97b588fa247e562177db84abba983b1
DanielSamuels <fishbonedaniel@gmail.com>
2022-12-25 17:19:03 -0800

    Update black_jack.py
    added the score and continuity

--- day_11_of_100/black_jack.py
+++ day_11_of_100/black_jack.py
@@ -2,12 +2,13 @@ from random import choice
 
 player_cards = []
 dealer_cards = []
-player_stake = 0
+player_stake = 100
 player_cash  = 1000
 player_score = 0
 dealer_score = 0
 cashout =False
 is_running = True
+play = True
 def deal_card():
   """ returns a random card from the deck"""
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -34,30 +35,90 @@ def check_cards(_player_cards,_dealer_cards):
             ace_flag -= 1
     return  _player_score ,_dealer_score       
 
-def show_stat():
+def show_stat(hide_dealer_card = False):
         """displays player score and cash """
+        global player_cards
+        global dealer_cards
         a,b = check_cards(player_cards,dealer_cards)
         print(f"player card: {player_cards}")   
-        print(f"player score: {a} player cash : {player_cash}")   
-        print(f"dealer cards:[*, {dealer_cards[1]}]") 
+        print(f"player score: {a} player cash : ${player_cash}") 
+        if hide_dealer_card:  
+            print(f"dealer cards:[*, {dealer_cards[0]}]") 
+        else:
+            print(f"dealer card: {dealer_cards},dealer score: {b}")
+
 
 def check_score(_player_score,_dealer_score):
-    pass  
+    if _player_score > 21:
+        return "bust" 
+    elif _player_score == _dealer_score:
+        return "draw"
+    elif _player_score > _dealer_score:
+        return "win" 
+    elif _dealer_score  > _player_score and _dealer_score <= 21:
+        return "lose"
+    else:
+        return "win"             
+     
 
 option = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
-if option == "y":
-  for i in range(2):
-    player_cards.append(deal_card())
-    dealer_cards.append(deal_card())
-  show_stat()
-while is_running:
-    if input("hit or stand? Type 'h' or 's': ") == 'h':
-        player_cards.append(deal_card())
-        show_stat()                    
+replay =False
+while play:
+    if replay:
+        if input("play again? Press any key  to play or 'c' to cashout: ") == "c":
+                        play = False
+                        print(f"You cashed out ${player_cash}")
+                        break  
+        else:
+            player_cards = []
+            dealer_cards = []    
+            # print("continue")  
+            # print(player_cards)
+            # print(dealer_cards)
+    replay = True                
 
-    else:
-         a,b = check_cards(player_cards,dealer_cards) 
-         while b < 17:
+    if option == "y":
+        for i in range(2):
+            player_cards.append(deal_card())
             dealer_cards.append(deal_card())
-            a,b = check_cards(player_cards,dealer_cards) 
-         print(a,b)  
+        show_stat(True)
+        is_running = True
+        while is_running :
+            if input("hit or stand? Type 'h' or 's': ") == 'h':
+                player_cards.append(deal_card())
+                show_stat(True)
+                a,b = check_cards(player_cards,dealer_cards)
+                if check_score(a,b) == "bust":
+                    print(f"BUST, You lost ${player_stake}")
+                    player_cash -= player_stake
+                    show_stat()      
+                    is_running = False             
+
+            else:
+                a,b = check_cards(player_cards,dealer_cards) 
+                while b < 17:
+                    dealer_cards.append(deal_card())
+                    a,b = check_cards(player_cards,dealer_cards) 
+                game_stat = check_score(a,b) 
+                if game_stat =="win":
+                    player_cash += player_stake
+                    print(f"You Won ${player_stake}")
+                elif game_stat =="draw":
+                    print(f"It's a Draw!!")
+                elif game_stat =="lose":
+                    player_cash -= player_stake 
+                    print(f"Oops, You lost ${player_stake}")   
+                    
+
+                show_stat()
+                # if input("play again? Press any key  to play or 'c' to cashout: ") == "c":
+                #         play = False
+                # else:
+                #     player_cards = []
+                #     dealer_cards = []    
+                #     print("continue")  
+                #     print(player_cards)
+                #     print(dealer_cards)
+                is_running = False
+    else:
+        play = False
