d31f2eb1c64baeeea35cd35a52fda0f6c82a49a8
Fishbone_Workstation <fishbone_workstation@pop-os.localdomain>
2023-01-04 00:16:39 +0100

    classes, attributes and methods

---
+++ day_17_of_100/oop.py
@@ -0,0 +1,17 @@
+
+class User:
+    def __init__(self, user_id, username) :
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self,user):
+        self.following += 1
+        user.followers += 1    
+
+user_1 = User("001", "Daniel")
+user_2 = User("002", "Fishbone")        
+
+user_1.follow(user_2)
+print(user_2.followers)
