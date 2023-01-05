5bc342b5b36a59ba574caa74ce815a10f9d40a6d
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-06 00:03:36 +0100

    added the 'random walk 'function, this function draws lines in random directions using random colors

--- day_18_of_100/polygon.py
+++ day_18_of_100/polygon.py
@@ -1,9 +1,16 @@
 from turtle import Turtle,Screen
-from random import choice 
+from random import choice, random
 
+screen = Screen()
 turtl = Turtle()
 turtl.shape("classic")
+direction = ["left", "forward", "right","backward"]
+
 colors = ["red","green","blue","yellow","dark green", "sienna","magenta" , "orange", "hot pink", "olive drab", "gray"]
+
+
+screen.colormode(1)
+
 def draw_polygon(_num_of_sides,_size = 100,_turtl = turtl):
     """ this draws a polygon, takes number of sides length of line and turtle object as inputs"""
 
@@ -25,13 +32,35 @@ def draw_dashed(_size,_turtl):
             
 
 def draw_all_shapes(_turtl):
+    """ this draws a dashed line, takes length of line in px and turtle object inputs"""
     for shape in range(3,9):
         _turtl.pencolor(choice(colors))
         draw_polygon(shape,100,_turtl)
 
 
+def random_walk(_steps,_turtl):
+    """makes several random movements of several colors"""
+    _turtl.pensize(10)
+    _turtl.speed(4)
+    for _step in range(_steps):
+        _dir = choice(direction)
+        R = random()
+        G = random()
+        B = random()
 
-draw_dashed(100,turtl)
-draw_all_shapes(turtl)
-screen = Screen()
+        _turtl.pencolor(R,G,B)
+        if _dir == "left":
+            _turtl.left(90)
+        elif _dir == "right":
+            _turtl.right(90)
+        elif _dir == "backward":
+            _turtl.right(180)    
+        _turtl.forward(30)    colors = ["red","green","blue","yellow","dark green", "sienna","magenta" , "orange", "hot pink", "olive drab", "gray"]
+
+
+
+
+random_walk(200,turtl)
+# draw_dashed(100,turtl)
+# draw_all_shapes(turtl)
 screen.exitonclick()
