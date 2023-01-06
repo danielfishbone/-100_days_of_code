43b99054f89517415c3b68ab9dc8a83c2eeac852
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-06 01:50:55 +0100

    cologram module picks the different colors in an image

---
+++ day_18_of_100/cologram.py
@@ -0,0 +1,17 @@
+import colorgram
+image_path = "/home/fishbone/-100_days_of_code/day_18_of_100/hirst1.jpeg"
+color_set = []
+def get_color(_path):
+    rgb = []
+    colors = colorgram.extract(_path,20)
+    for color in colors:
+        x = color.rgb
+        r = x.r
+        g = x.g
+        b = x.b
+        color_element = (r,g,b)
+        rgb.append(color_element)
+    return rgb
+    
+color_set = get_color(image_path)
+print(color_set)   
