f48da53edcda7e8a843d698cb82b30b14a4ba774
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-07 19:14:35 +0100

    finishing touches

--- day_19_of_100/turtle_race.py
+++ day_19_of_100/turtle_race.py
@@ -11,16 +11,9 @@ purple_turtle = Turtle()
 orange_turtle = Turtle()
 indigo_turtle = Turtle()
 
-red_turtle.color("red")
-yellow_turtle.color("yellow")  
-green_turtle.color("green")  
-blue_turtle.color("blue")
-purple_turtle.color("purple") 
-orange_turtle.color("orange")
-indigo_turtle.color("indigo")
-
 turtles = (red_turtle,yellow_turtle,green_turtle,blue_turtle,purple_turtle,orange_turtle,indigo_turtle)
 turtle_colors = ("red","yellow","green","blue","purple","orange","indigo")
+
 def set_home():
     home_x = -200
     home_y = -150
@@ -28,6 +21,7 @@ def set_home():
     
     for i in range(len(turtles)):
         turtles[i].shape("turtle")
+        turtles[i].color(turtle_colors[i])
         turtles[i].speed("fastest")
         turtles[i].penup()
         turtles[i].setheading(0)
@@ -42,27 +36,17 @@ def race(_user_bet):
             turtles[index].forward(randint(1,10))
             if turtles[index].xcor() >= 200:
                 if turtle_colors[index]==_user_bet.lower():
-                    print (f"Congratulations!, You WON the bet {turtle_colors[index].capitalize()} Turtle won")
+                    print (f"Congratulations!, You WON the bet, {turtle_colors[index].capitalize()} Turtle won")
+                    screen.title(f"Congratulations!, You WON the bet, {turtle_colors[index].capitalize()} Turtle won  ")
                 else:
-                    print(f"Oops!, You LOST the bet {turtle_colors[index].capitalize()} Turtle won")    
+                    print(f"Oops!, You LOST the bet, {turtle_colors[index].capitalize()} Turtle won")   
+                    screen.title(f"Oops!, You LOST the bet, {turtle_colors[index].capitalize()} Turtle won  ") 
                 is_runnung = False
                 break
-
-
-
-
-
-
 
 screen.setup(width=500,height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle would win?, Enter your color")
 
 set_home()
 race(user_bet)
-
-
-
-
-
-
 screen.exitonclick()
