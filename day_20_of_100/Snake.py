1ff35955075dd8362781284aee442dd0b8ff4dc6
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-08 23:19:12 +0100

    added 'move 'method for snake

--- day_20_of_100/Snake.py
+++ day_20_of_100/Snake.py
@@ -1,7 +1,8 @@
 from random import randint
 import time
 from turtle import Turtle, Screen
-
+screen = Screen()
+screen.bgcolor("black")
 
 class Snake:
     def __init__(self) -> None:
@@ -14,7 +15,15 @@ class Snake:
             seg = Turtle("square")
             seg.color("white")
             seg.penup()
+            seg.goto((-i*-20)-40,0)
             self.snake.append(seg)
+    def move(self):
+            for seg_num in range(len(self.snake)-1,0,-1):
+                new_x = self.snake[seg_num-1].xcor()
+                new_y = self.snake[seg_num-1].ycor()
+                self.snake[seg_num].goto(x = new_x, y = new_y)
+            self.snake[0].fd(10)
 
-        
-        
+
+snake1 = Snake()       
+screen.exitonclick() 
