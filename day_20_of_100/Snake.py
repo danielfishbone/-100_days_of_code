c6cab3a1fc6091db6e2d2cd7b233fb5470b190ed
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-09 00:18:05 +0100

    added 'direction 'methods for snake class

--- day_20_of_100/Snake.py
+++ day_20_of_100/Snake.py
@@ -1,9 +1,7 @@
-from random import randint
 import time
 from turtle import Turtle, Screen
-screen = Screen()
-screen.bgcolor("black")
-screen.tracer(0)
+
+
 class Snake:
     def __init__(self) -> None:
         seg = Turtle("square")
@@ -38,19 +36,35 @@ class Snake:
                 new_y = self.snake[seg_num-1].ycor()
                 self.snake[seg_num].goto(x = new_x, y = new_y)
             self.snake[0].fd(20)
-
+    
+    def go_left(self):
+        if self.snake[0].heading() != 0: 
+            self.snake[0].setheading(180)
+    def go_right(self):
+        if self.snake[0].heading() != 180: 
+            self.snake[0].setheading(0)
+    def go_up(self):
+        if self.snake[0].heading() != 270: 
+            self.snake[0].setheading(90)
+    def go_down(self):
+        if self.snake[0].heading() != 90: 
+            self.snake[0].setheading(270)
 
 
 
 
 if __name__ == "__main__":
+    screen = Screen()
+    screen.bgcolor("black") # set background colour to black
+    screen.tracer(0) # Turn off automatic screen updates
     snake1 = Snake() 
-    c = 0    
     while True:
+        screen.listen()
+        screen.onkeypress(key="Left",fun=snake1.go_left)
+        screen.onkeypress(key="Right",fun=snake1.go_right)
+        screen.onkeypress(key="Up",fun=snake1.go_up)
+        screen.onkeypress(key="Down",fun=snake1.go_down)
         snake1.move()
         screen.update()
-        if c % 10 == 0:
-            snake1.add_segment()
-        time.sleep(1)
-        c += 1
-    screen.exitonclick() 
+
+        time.sleep(0.1)
