63a82d060175d04e5244945bf11218a696f56f17
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-08 21:51:19 +0100

    added snake size incrementation using the 'add_segment' function

--- day_20_of_100/snake.py
+++ day_20_of_100/snake.py
@@ -25,7 +25,7 @@ def move_food(_food):
 
 def draw_boundary():
     bt = Turtle()
-    bt.color("white")
+    bt.color("red")
     bt.penup()
     bt.goto(-1*width/2,-1*height/2)
     bt.pendown()
@@ -35,19 +35,28 @@ def draw_boundary():
     bt.goto(-1*width/2,-1*height/2)
     bt.ht()
 
+def new_segment(_snake,_x,_y):
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.setpos(_x,_y)
+    _snake.append(new_segment)
+
+def add_segment(_snake):
+    x_val = _snake[-2].xcor()-_snake[-1].xcor()
+    y_val = _snake[-2].ycor()-_snake[-1].ycor()
+    x = _snake[-1].xcor() + (-1*x_val)
+    y = _snake[-1].ycor() + (-1*y_val)
+    new_segment(_snake,x,y)
+
 x = y = 0
 
 food_flag = False
 score = 0
 draw_boundary()
 for _ in range(5):
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.setpos(x,y)
-    x-=20
-    snake.append(new_segment)
-    
+    new_segment(snake,x,y)
+    x-=20    
 screen.setup(width=width,height=height)
 screen.bgcolor("black")
 screen.title("Snakes")
@@ -62,7 +71,7 @@ while not game_over:
     screen.onkeypress(key="Left",fun=move_left)
     screen.onkeypress(key="Right",fun=move_right)
     
-    snake[0].fd(20)
+    snake[0].fd(10)
     for seg_num in range(len(snake)-1,0,-1):
         new_x = snake[seg_num-1].xcor()
         new_y = snake[seg_num-1].ycor()
@@ -74,6 +83,7 @@ while not game_over:
             if ( snake[0].xcor() >= food.xcor()-10 and snake[0].xcor()  <= food.xcor()+10 ) and ( snake[0].ycor() >= food.ycor()-10 and snake[0].ycor() <= food.ycor() +10 ):
                 score += 1  
                 screen.title(score) 
+                add_segment(snake)
                 food_flag = False
 
 screen.exitonclick()
