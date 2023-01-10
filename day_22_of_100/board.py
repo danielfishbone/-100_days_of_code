af4839eeb396f5771b0cef1efb0c652b5f6c9f17
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-10 18:08:29 +0100

    created draw centre line  method for the board class

--- day_22_of_100/board.py
+++ day_22_of_100/board.py
@@ -1,14 +1,20 @@
-from turtle import Turtle
-BOARD_COLOR = "white"
-PENSIZE = 10
-PEN_COLOR = "red"
+from turtle import Turtle, Screen, width
+PENSIZE = 5
+PEN_COLOR = "yellow"
+FILL_COLOR = "white"
+SPACING = 10
+
 class Board(Turtle):
-    def __init__(self,_width,_height):
+    def __init__(self,width,height):
         super().__init__(visible = False)
-        self.width = _width/2 
-        self.height = _height/2
-        self.pen(fillcolor=BOARD_COLOR, pencolor=PEN_COLOR, pensize=PENSIZE)
+        self.width = width/2 
+        self.height = height/2
+        self.pen(fillcolor=FILL_COLOR, pencolor=PEN_COLOR, pensize=PENSIZE)
+        self.speed("fastest")
         self.penup()
+        self.draw_board()
+        self.draw_centre_line()
+
     def draw_board(self):
         self.goto(-self.width,-self.height)
         self.pendown()
@@ -19,7 +25,30 @@ class Board(Turtle):
         self.penup()
             
     def draw_centre_line(self):
+        _size = self.height*2
+        _num_of_pace = int(_size/SPACING)
+
         self.goto(0,-self.height)
         self.pendown()
+        current_heading = self.heading() 
+        self.setheading(90)
+        for i in range(_num_of_pace):
+            if i%2 == 0:
+                self.penup()
+                self.forward(SPACING)
+            else:
+                self.pendown()
+                self.forward(SPACING)
+
         self.goto(0,self.height)
+        self.penup()
+        self.setheading(current_heading)
+
+
+if __name__ == "__main__":
+    screen = Screen()
+    screen.bgcolor("black")
+    board =Board(width=800,height = 600)
+
+    screen.exitonclick()
 
