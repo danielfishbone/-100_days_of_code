753ef509959d9687c5cbbb6de36971ee0b95f926
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-10 17:42:26 +0100

    created draw board method for the board class

--- day_22_of_100/board.py
+++ day_22_of_100/board.py
@@ -3,9 +3,19 @@ BOARD_COLOR = "white"
 PENSIZE = 10
 PEN_COLOR = "red"
 class Board(Turtle):
-    def __init__(self):
+    def __init__(self,_width,_height):
         super().__init__(visible = False)
+        self.width = _width/2 
+        self.height = _height/2
         self.pen(fillcolor=BOARD_COLOR, pencolor=PEN_COLOR, pensize=PENSIZE)
-
-
-
+        self.penup()
+    def draw(self):
+        self.goto(-self.width,-self.height)
+        self.pendown()
+        self.goto(self.width,-self.height)
+        self.goto(self.width,self.height)
+        self.goto(-self.width,self.height)
+        self.goto(-self.width,-self.height)
+        self.penup()
+            
+   
