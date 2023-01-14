420a280d9d293fd5f8c2f26d529cc5ccb98dd25d
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-14 14:01:49 +0100

    added pause function to the main game

--- day_23_of_100/main.py
+++ day_23_of_100/main.py
@@ -13,48 +13,57 @@ screen.setup(WIDTH,HEIGHT)
 screen.tracer(0)
 player = Player()
 scoreboard = ScoreBoard()
+cars.append(Car())
+
+
 sleep_time = 0.1
 new_car_flag = False
 count = 0
 counter = 0
-cars.append(Car())
+paused =False
 game_over = False
+
+def pause():
+    global paused
+    paused = not paused
 while not game_over:
 
-
-    if new_car_flag:
-        count = randint(1,10)
-        new_car_flag =False
-
-    if counter>=count:
-        cars.append(Car())
-        counter = 0
-        new_car_flag = True
-
-    if player.ycor()>= 280:
-        scoreboard.level_up()
-        player.reset_position()
-        sleep_time * 0.66
-        if sleep_time < 0.01:
-            sleep_time = 0.01    
-
-    for i in range(len(cars)):
-        cars[i].move()
-        if player.distance(cars[i]) <10:
-            game_over = True
-            scoreboard.game_over()
-        if cars[i].xcor() <= -350:
-            cars[i].hideturtle()
-            cars[i] = None
-            cars.pop(i) 
-            break   
-    
     screen.listen()
-    screen.onkeypress(fun =player.move, key="Up" )
+    screen.onkeypress(fun =pause, key="space" )
+    if not paused:
+        screen.onkeypress(fun =player.move, key="Up" )
+        if new_car_flag:
+            count = randint(1,10)
+            new_car_flag =False
+
+        if counter>=count:
+            cars.append(Car())
+            counter = 0
+            new_car_flag = True
+
+        if player.ycor()>= 280:
+            scoreboard.level_up()
+            player.reset_position()
+            sleep_time * 0.66
+            if sleep_time < 0.01:
+                sleep_time = 0.01    
+
+        for i in range(len(cars)):
+            cars[i].move()
+            if player.distance(cars[i]) <10:
+                game_over = True
+                scoreboard.game_over()
+            if cars[i].xcor() <= -350:
+                cars[i].hideturtle()
+                cars[i] = None
+                cars.pop(i) 
+                break   
+        
+        
 
 
 
-    counter += 1
+        counter += 1
     screen.update()
     sleep(sleep_time)
 screen.exitonclick()
