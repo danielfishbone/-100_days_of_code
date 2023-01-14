c87d29830afd64053388a93d7e83f2aecf474087
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-14 13:13:19 +0100

    updated scoreboard attributes

--- day_23_of_100/scoreboard.py
+++ day_23_of_100/scoreboard.py
@@ -1,11 +1,15 @@
 from turtle import Turtle
 
 FONT = ("Courier", 24, "normal")
-
+SCORE_POS = (-200,250)
 
 class ScoreBoard(Turtle):
     def __init__(self) -> None:
         super().__init__(visible = False)
         self.score = 0
+        self.penup()
+        self .goto(SCORE_POS)
+        self.color("black")
+        self.write(self.score,align="center",font=FONT)
 
     
