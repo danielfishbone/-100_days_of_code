938f880edb3d3b64a7fa9c1c8bffb1bddf1e245b
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-16 00:49:05 +0100

    completed  day 24 challenge

--- day_24_of_100/main.py
+++ day_24_of_100/main.py
@@ -7,8 +7,22 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-with open("/home/fishbone/python_100_days_of_code/day_24_of_100/Input/Names/invited_names.txt") as name_file:
+from cgitb import text
 
+
+with open("/home/fishbone/python_100_days_of_code/day_24_of_100/Input/Names/invited_names.txt") as name_file:
     names = name_file.readlines()
-print(names)
+with open("/home/fishbone/python_100_days_of_code/day_24_of_100/Input/Letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
+
+stripped_names =[]
+for name in names:
+    stripped_names.append(name.strip())
+
+  
+for name in stripped_names:
+    new_letter = letter.replace("[name]", name)
+    with open(f"/home/fishbone/python_100_days_of_code/day_24_of_100/Output/ReadyToSend/letter_for_{name}.txt","w")as ready_letter:
+        ready_letter.write(new_letter)
+
 
