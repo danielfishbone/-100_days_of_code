1185d5254ef0d7b19afa131e2129115c63ce5c44
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-15 22:47:21 +0100

    added highscore attribute to the scoreboard class in day 20 snake game

--- day_20_of_100/scoreboard.py
+++ day_20_of_100/scoreboard.py
@@ -6,6 +6,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__(visible=False)
         self.score = 0
+        self.high_score=0
         self.penup()
         self.color("white")
         self.goto(0,270)
@@ -18,7 +19,7 @@ class ScoreBoard(Turtle):
         self.write(f"GAME OVER",align="center",font=("Arial",20,"bold")) 
     def update (self):
         self.clear()
-        self.write(f"SCORE : {self.score} ",align="center",font=("Arial",20,"bold"))   
+        self.write(f"SCORE : {self.score} HIGHSCORE{self.high_score}",align="center",font=("Arial",20,"bold"))   
         
 
 if __name__ =="__main__":
