31a75e5302c99705cec3d5640664eac5887b6fbf
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-16 00:32:57 +0100

    created files for day 24 challenge

---
+++ day_24_of_100/main.py
@@ -0,0 +1,14 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("/home/fishbone/python_100_days_of_code/day_24_of_100/Input/Names/invited_names.txt") as name_file:
+
+    names = name_file.readlines()
+print(names)
+
