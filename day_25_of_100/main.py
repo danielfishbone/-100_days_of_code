d8827ca935074c4b04c5c26d1d41eb5287eac89b
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-19 22:13:39 +0100

    pandas

--- day_25_of_100/main.py
+++ day_25_of_100/main.py
@@ -1,6 +1,22 @@
+import csv
+import pandas
+
+
+# with open("/home/fishbone/python_100_days_of_code/day_25_of_100/weather_data.csv") as file:
+#     data = file.readlines()
+
+
 
 with open("/home/fishbone/python_100_days_of_code/day_25_of_100/weather_data.csv") as file:
-    data = file.readlines()
+    data = csv.reader(file)
+    temperatures = []
+    for row in data:
+        if row[1].isnumeric():
+            temperatures.append(int(row[1]))
 
 
-print(data)    
+print(temperatures)        
+
+new_data = pandas.read_csv("/home/fishbone/python_100_days_of_code/day_25_of_100/weather_data.csv") 
+
+print(new_data["temp"])
