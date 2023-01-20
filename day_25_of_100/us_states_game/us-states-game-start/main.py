4edc566c94dab5d90f91c968406a88507462c03a
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-20 01:37:25 +0100

    added logic to check input and compare with database

--- day_25_of_100/us_states_game/us-states-game-start/main.py
+++ day_25_of_100/us_states_game/us-states-game-start/main.py
@@ -1,13 +1,15 @@
 # from turtle import Screen,Turtle
+from asyncore import write
 import turtle
 import pandas as pd
-
+from writer import Writer
 
 states = []
 x_coordinates = []
 y_coordinates = []
 
 screen = turtle.Screen()
+writer = Writer()
 screen.title("US GAME")
 screen.bgpic("day_25_of_100/us_states_game/us-states-game-start/blank_states_img.gif")
 screen.setup(width = 730,height = 500)
@@ -15,15 +17,22 @@ screen.setup(width = 730,height = 500)
 data = pd.read_csv("day_25_of_100/us_states_game/us-states-game-start/50_states.csv")
 
 states_obj = data.state
+x_obj = data.x
+y_obj = data.y
 
 states = states_obj.to_list()
+x_coordinates =  x_obj.to_list()
+y_coordinates  =  y_obj.to_list()
 
-print(states)
+new_guess = screen.textinput(title = "Guess the States", prompt = "Whats your next guess?")
+new_guess = new_guess.capitalize()
 
-# new_guess = screen.textinput(title = "Guess the States", prompt = "Whats your next guess?")
-# new_guess = new_guess.capitalize()
-
-
+if new_guess in states:
+    index = states.index(new_guess)
+    cor_x = x_coordinates[index]
+    cor_y = y_coordinates[index]
+    writer.goto(cor_x,cor_y)
+    writer.write(new_guess,align="center")
 
 
 turtle.mainloop()
