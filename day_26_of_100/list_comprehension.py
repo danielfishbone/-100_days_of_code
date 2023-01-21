40787c570e75a7f431d13a353d502faf31372cc4
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-22 00:24:15 +0100

    list comprehension

--- day_26_of_100/list_comprehension.py
+++ day_26_of_100/list_comprehension.py
@@ -6,5 +6,10 @@ print(new_numbers)
 # for this list comprehension we want to print every element of the new_numbers list  
 
 x =[print(i)for i in new_numbers] 
-
 print (x)
+
+[print(i)for i in "Random String"] # can work with strings too
+
+new_list = [n*2 for n in range(1,5)]
+
+print(new_list)
