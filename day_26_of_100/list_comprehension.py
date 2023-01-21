829bc2597167661b6a849b904e74ea176c57c03a
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-22 00:18:51 +0100

    list comprehension

---
+++ day_26_of_100/list_comprehension.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3]
+# for this list comprehension we want to multiply every element of the numbers list by a numvber (3)
+new_numbers = [i*3 for i in numbers]
+print(new_numbers)
+
+# for this list comprehension we want to print every element of the new_numbers list  
+
+x =[print(i)for i in new_numbers] 
+
+print (x)
