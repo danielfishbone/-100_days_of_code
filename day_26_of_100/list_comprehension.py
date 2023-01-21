422ab59c6c39b42a8074dfe54a9b788009af2c69
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-22 00:56:04 +0100

    more examples on string comprehension

--- day_26_of_100/list_comprehension.py
+++ day_26_of_100/list_comprehension.py
@@ -20,3 +20,20 @@ print(new_list)
 #  
 odd_nums = [n for n in range(1,20) if n %2 != 0]
 print(odd_nums)
+# ************************************************************************
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+squared_numbers = [n*n for n in numbers]
+print(squared_numbers)
+# ******************************************************
+# the following example demonstrates checking for numbers common in two files and add to a new list using list comprehension
+with open("file1.txt") as f1:
+    list_1 = f1.readlines()
+with open("file2.txt") as f2:
+    list_2 = f2.readlines()
+
+c = [int(x.strip()) for x in list_1 if x in list_2 ]
+
+# Write your code above 👆
+print(c )
+
