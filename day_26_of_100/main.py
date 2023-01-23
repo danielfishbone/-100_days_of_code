7db3ff6ef148b61dcd9f523f219d2b5eef317ce8
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-24 00:22:23 +0100

    uding iterrows to loop through the dataframe

--- day_26_of_100/main.py
+++ day_26_of_100/main.py
@@ -1,2 +1,17 @@
 import pandas as pd
 
+df = pd.read_csv("day_26_of_100/nato_phonetic_alphabet.csv")
+word = input("Type the word you want to spell using the nato alphabet: ").capitalize()
+print (df)
+
+letter = df.letter
+code = df.code
+
+
+
+data_dict = {row.letter:row.code for(index,row) in df.iterrows()}
+
+alpa_list = []
+
+
+
