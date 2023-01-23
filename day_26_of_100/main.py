c60b7640f6fd8811e5f7b7218f13330e2a852a23
DanielSamuels <fishbonedaniel@gmail.com>
2023-01-24 00:32:08 +0100

    day 26 challenge completed

--- day_26_of_100/main.py
+++ day_26_of_100/main.py
@@ -4,15 +4,11 @@ df = pd.read_csv("day_26_of_100/nato_phonetic_alphabet.csv")
 word = input("Type the word you want to spell using the nato alphabet: ").upper()
 # print (df)
 
-letter = df.letter
-code = df.code
-
 
 #  using iterrows to loop through the dataframe 
 data_dict = {row.letter:row.code for(index,row) in df.iterrows()}
-# print(data_dict)
 
-
+# 
 alpa_list = [data_dict[key] for key in word]
 
 print(alpa_list)
