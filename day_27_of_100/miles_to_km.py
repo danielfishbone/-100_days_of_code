33c9fa41c38600278c9a35dd42d5faf380940e43
DanielSamuels <fishbonedaniel@gmail.com>
2023-02-08 11:57:53 +0100

    day 27 challenge complete

--- day_27_of_100/miles_to_km.py
+++ day_27_of_100/miles_to_km.py
@@ -1,23 +1,34 @@
 import tkinter 
 def on_button():
-    pass
+    global answer
+    try:
+        km =int(input.get())
+        answer =km*0.621371
+        answer_label.config(text=answer)
+    except:    
+        pass
 window = tkinter.Tk()
-window.title("Mile to Km Converter")
+window.title("Km to Mile Converter")
 window.minsize(width=350,height=100)
+answer = 0
 
 mile_label = tkinter.Label(text="Miles",font=("Arial",16,"normal"))
-mile_label.grid(row=2,column=3)
+mile_label.grid(row=1,column=5)
 
 km_label = tkinter.Label(text="Km",font=("Arial",16,"normal"))
-km_label.grid(row=1,column=3)
+km_label.grid(row=1,column=2)
 
-is_equal_label = tkinter.Label(text="is equal to",font=("Arial",16,"normal"))
-is_equal_label.grid(row=2,column=1)
+is_equal_label = tkinter.Label(text=" = ",font=("Arial",16,"normal"))
+is_equal_label.grid(row=1,column=3)
+
+answer_label = tkinter.Label(text=str(answer),font=("Arial",16,"normal"))
+answer_label.grid(row=1,column=4)
 
 input = tkinter.Entry()
-input.grid(row=1,column=2)
+input.config(width=6)
+input.grid(row=1,column=1)
 
 button = tkinter.Button(text="Calculate",command=on_button)
-button.grid(row=3,column=2)
+button.grid(row=3,column=3)
 
 window.mainloop() 
