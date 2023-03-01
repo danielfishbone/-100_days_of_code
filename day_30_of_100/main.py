0739d411477a35a6a6ffd1ed159e3a963e0cf5f2
DanielSamuels <fishbonedaniel@gmail.com>
2023-03-01 09:50:10 +0100

    day 30 updated  password manager to handle invalid inputs

---
+++ day_30_of_100/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.read_csv("day_26_of_100/nato_phonetic_alphabet.csv")
+# print (df)
+
+
+#  using iterrows to loop through the dataframe 
+data_dict = {row.letter:row.code for(index,row) in df.iterrows()}
+
+#
+def get_word():
+    word = input("Type the word you want to spell using the nato alphabet: ").upper()
+    try: 
+        alpa_list = [data_dict[key] for key in word]
+        
+
+    except KeyError:
+        print("Sorry only letters are allowed")
+        get_word()
+    else:
+        print(alpa_list)
+get_word()
+
+
