670fadb18a8a6144f1f80b8b1a1f1c86940019f1
DanielSamuels <fishbonedaniel@gmail.com>
2023-03-01 11:57:18 +0100

    Error handling

--- day_29_of_100/main.py
+++ day_29_of_100/main.py
@@ -1,109 +1,115 @@
 from tkinter import *
 from tkinter import messagebox
-import pyperclip 
+import pyperclip
 from random import choice
 from random import randint
-from password import letters,numbers,symbols
+from password import letters, numbers, symbols
 import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
 def on_gen_button():
-  nr_letters = 3
-  nr_symbols = 2
-  nr_numbers = 3 
+    nr_letters = 3
+    nr_symbols = 2
+    nr_numbers = 3
 
-  list_of_char = []
-  for sample in range(nr_letters):
-      list_of_char.append("L")
-  for sample in range(nr_symbols):
-      list_of_char.append("S")
-  for sample in range(nr_numbers):
-      list_of_char.append("N")
-  password = ""
+    list_of_char = []
+    for sample in range(nr_letters):
+        list_of_char.append("L")
+    for sample in range(nr_symbols):
+        list_of_char.append("S")
+    for sample in range(nr_numbers):
+        list_of_char.append("N")
+    password = ""
 
-  for i in range(len(list_of_char)):
-      selected = randint(0, len(list_of_char) - 1)
-      val = list_of_char.pop(selected)
-      if val == "L":
-          password += str(choice(letters))
-      elif val == "S":
-          password += str(choice(symbols))
-      elif val == "N":
-          password += str(choice(numbers))
-  password_input.delete(0,END) 
-  password_input.insert(0,password) 
-  pyperclip.copy(password)
-  
+    for i in range(len(list_of_char)):
+        selected = randint(0, len(list_of_char) - 1)
+        val = list_of_char.pop(selected)
+        if val == "L":
+            password += str(choice(letters))
+        elif val == "S":
+            password += str(choice(symbols))
+        elif val == "N":
+            password += str(choice(numbers))
+    password_input.delete(0, END)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
 
-    
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_input.get()
     username = username_input.get()
     password = password_input.get()
     data_dict = {
-      website:{
-        "username":username,
-        "password":password,
-      }
+        website: {
+            "username": username,
+            "password": password,
+        }
     }
 
-    is_ok = messagebox.askokcancel(title=website,message=f"website:{website}\npassword: {password} \nusername: {username}\nDo you wish to save? ")
+    is_ok = messagebox.askokcancel(
+        title=website, message=f"website:{website}\npassword: {password} \nusername: {username}\nDo you wish to save? ")
 
-    if is_ok: 
-      if len(website) != 0 and  len(username) != 0 and len(password) != 0:
-        try:
-          with open("day_29_of_100/data.json", mode = "r") as file :
-            data = json.load(file)
-            data.update(data_dict)
-        except:
-           with open("day_29_of_100/data.json", mode = "w") as file :
-            json.dump(data_dict,file,indent=4)   
+    if is_ok:
+        if len(website) != 0 and len(username) != 0 and len(password) != 0:
+            try:
+                with open("day_29_of_100/data.json", mode="r") as file:
+                    data = json.load(file)
+                    data.update(data_dict)
+            except FileNotFoundError:
+                with open("day_29_of_100/data.json", mode="w") as file:
+                    json.dump(data_dict, file, indent=4)
+            except json.decoder.JSONDecodeError:
+                with open("day_29_of_100/data.json", mode="w") as file:
+                    json.dump(data_dict, file, indent=4)
+            else:
+                with open("day_29_of_100/data.json", mode="w") as file:
+                    json.dump(data, file, indent=4)
+            website_input.delete(0, END)
+            password_input.delete(0, END)
         else:
-          with open("day_29_of_100/data.json", mode = "w") as file :
-            json.dump(data,file,indent=4)
-        website_input.delete(0,END) 
-        password_input.delete(0,END) 
-      else:
-        messagebox.showwarning(title="Error Saving",message="please ensure none of the fields are empty")  
-    
+            messagebox.showwarning(
+                title="Error Saving", message="please ensure none of the fields are empty")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
- 
-window =  Tk()
+
+window = Tk()
 window.config(
-padx=50,pady=50)
+    padx=50, pady=50)
 window.title("Password Manager")
-canvas = Canvas(width=200,height=200, highlightthickness=0)
+canvas = Canvas(width=200, height=200, highlightthickness=0)
 
 image_path = "day_29_of_100/logo.png"
-image = PhotoImage(file = image_path)
-canvas.create_image(100,100,image = image)
-canvas.grid(row=0,column=1)
-website_label =Label(text="Website: ",font=("Arial",16,"normal"))
-website_label.grid(row=1,column=0)
-username_label =Label(text="Email/Username: ",font=("Arial",16,"normal"))
-username_label.grid(row=2,column=0)
-password_label =Label(text="password: ",font=("Arial",16,"normal"))
-password_label.grid(row=3,column=0)
+image = PhotoImage(file=image_path)
+canvas.create_image(100, 100, image=image)
+canvas.grid(row=0, column=1)
+website_label = Label(text="Website: ", font=("Arial", 16, "normal"))
+website_label.grid(row=1, column=0)
+username_label = Label(text="Email/Username: ", font=("Arial", 16, "normal"))
+username_label.grid(row=2, column=0)
+password_label = Label(text="password: ", font=("Arial", 16, "normal"))
+password_label.grid(row=3, column=0)
 
 website_input = Entry()
 website_input.config(width=38)
-website_input.grid(row=1,column=1, columnspan=2)
+website_input.grid(row=1, column=1, columnspan=2)
 website_input.focus()
 
 username_input = Entry()
 username_input.config(width=38)
-username_input.grid(row=2,column=1, columnspan=2)
-username_input.insert(0,"samuelscdaniel@gmail.com")
-password_input =  Entry()
+username_input.grid(row=2, column=1, columnspan=2)
+username_input.insert(0, "samuelscdaniel@gmail.com")
+password_input = Entry()
 password_input.config(width=24)
-password_input.grid(row=3,column=1)
+password_input.grid(row=3, column=1)
 
-generate_button = Button(text="Generate",command=on_gen_button, width=10)
-generate_button.grid(row=3,column=2)
+generate_button = Button(text="Generate", command=on_gen_button, width=10)
+generate_button.grid(row=3, column=2)
 
-add_button = Button(text="Add",command=save_password, width=36)
-add_button.grid(row=4,column=1, columnspan=2)
+add_button = Button(text="Add", command=save_password, width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
